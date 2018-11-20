21793a09b8bcd206006042d40a5316f2435fb11f
sricheta92 <srichetaruj.iste@gmail.com>
2018-11-19 20:19:58 -0800

    added list files rpc

--- src/main/java/coordination/ExternalFileTransferImpl.java
+++ src/main/java/coordination/ExternalFileTransferImpl.java
@@ -3,7 +3,15 @@ package coordination;
 import grpc.DataTransferServiceGrpc;
 import grpc.FileTransfer;
 import io.atomix.AtomixClient;
+import io.atomix.collections.DistributedMap;
 import io.grpc.stub.StreamObserver;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class ExternalFileTransferImpl extends DataTransferServiceGrpc.DataTransferServiceImplBase {
 
@@ -34,12 +42,27 @@ public class ExternalFileTransferImpl extends DataTransferServiceGrpc.DataTransf
 	//UploadFile (Not supported yet/between external client and proxy server)
 
 	//ListFiles (between any client and This coordination server)
-	public void ListFiles(FileTransfer.RequestFileList request, StreamObserver<FileTransfer.FileList> responseObserver){
+	public void ListFiles(FileTransfer.RequestFileList request, StreamObserver<FileTransfer.FileList> responseObserver) throws ExecutionException, InterruptedException {
+		CompletableFuture<DistributedMap<Object, Object>> map = raftClient.getMap("fileLocations");
+		Iterator<Map.Entry<Object, Object>> it = raftClient.getMap("fileLocations")
+				.thenCompose(m -> m.entrySet())
+				.thenApply(a -> a.iterator())
+				.get();
+		List<String> list = new ArrayList<String>();
+		while(it.hasNext()){
+			Map.Entry<Object, Object> entry  = it.next();
+			String[] value = ((String)entry.getValue()).split(",");
+			list.add(value[1]);
+		}
+
+		//TODO how to create a builder
 
 	}
 
 	//RequestFileUpload (though uploading is not supported, just return a list of all proxies)
 	public void RequestFileUpload(FileTransfer.FileUploadInfo request, StreamObserver<FileTransfer.ProxyList> responseObserver){
+
+
 
 	}
 }
